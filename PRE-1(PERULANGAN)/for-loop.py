383d1a197b2d5b8be6278c74cb2f2d3d9331cf83
Ramadhan2212 <ramadhan72679@gmail.com>
2025-04-08 13:51:57 +0800

    Add files via upload

--- PRE-1(PERULANGAN)/for-loop.py
+++ PRE-1(PERULANGAN)/for-loop.py
@@ -1,1 +1,31 @@
+# perulangan (loop)
 
+# for kondisi: JIKA KONDISI TERPENUHI 
+#     aksi  JALANKAN AKSI 
+
+angka_list = {0,2,4,8,10} # ini adalah list 
+print(angka_list)
+
+# angka = 0
+# print(angka)
+# angka += 1 # angka = angka + 1
+# print(angka)
+# angka += 1
+# print(angka)
+
+# for-loop
+for i in angka_list:
+    print(f"i sekarang -> {i}")
+
+print("loop berakhir \n")
+
+# dengan range
+for i in range(5):
+    print(f"i sekarang -> {i}")
+
+print("loop range berakhir \n")
+
+for i in range(1,10):
+    print(f"i sekarang -> {i}")
+
+print("loop range berakhir \n")
