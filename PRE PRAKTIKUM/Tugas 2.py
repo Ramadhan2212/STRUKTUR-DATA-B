ca431e35b4ac81e6ca007752cd75ced5a87bcdc4
Ramadhan2212 <ramadhan72679@gmail.com>
2025-04-22 08:59:32 +0800

    Add files via upload

--- PRE PRAKTIKUM/Tugas 2.py
+++ PRE PRAKTIKUM/Tugas 2.py
@@ -1,10 +1,3 @@
-# Meminta input dari pengguna
-jumlah_deret = int(input("Masukkan jumlah deret bilangan genap: "))
-
-print("Output:", end=" ")
-
-# Menggunakan loop untuk mencetak bilangan genap
-bilangan = 2
-for i in range(jumlah_deret):
-    print(bilangan, end=", " if i < jumlah_deret - 1 else "")
-    bilangan += 2
+angka = int(input('angga yang kamu inginkan: '))
+for i in range(2, angka + 1, 2):
+    print(i)
